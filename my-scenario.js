fc834b9147fd04145a768fab8cb738ca342711b0
shyamala2 <131164564+shyamala2@users.noreply.github.com>
2024-04-09 13:46:40 +0530

    Update my-scenario.js

--- my-scenario.js
+++ my-scenario.js
@@ -3,7 +3,11 @@ const visit = async (page) => {
     await page.goto('', { waitUntil: 'networkidle' }); // Go to the baseUrl
     await page.waitForTimeout(3000); // Wait for 3 seconds
     //await page.scrollToElement('About Us'); // Scroll to the footer (if present)
-     await page.scrollToEnd();
+     //await page.scrollToEnd();
+    await page.fill("input[id='twotabsearchtextbox']", "oneplus");
+    await page.click("input[id='nav-search-submit-button']");
+    await page.waitForTimeout(3000);
+    await page.click("a[class='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']");
     await page.waitForNetworkIdle(); // Wait every request has been answered as a normal user.
 };
 
