d260eed708bc976bb7af5b1129f442282ad53418
shyamala2 <131164564+shyamala2@users.noreply.github.com>
2024-04-09 13:39:52 +0530

    Update my-scenario.js

--- my-scenario.js
+++ my-scenario.js
@@ -2,7 +2,8 @@
 const visit = async (page) => {
     await page.goto('', { waitUntil: 'networkidle' }); // Go to the baseUrl
     await page.waitForTimeout(3000); // Wait for 3 seconds
-    await page.scrollToElement('About Us'); // Scroll to the footer (if present)
+    //await page.scrollToElement('About Us'); // Scroll to the footer (if present)
+     await page.scrollToEnd();
     await page.waitForNetworkIdle(); // Wait every request has been answered as a normal user.
 };
 
