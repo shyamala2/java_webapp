428ea5644a4efb75fb9ce5930161c7781afdc632
shyamala2 <131164564+shyamala2@users.noreply.github.com>
2024-04-09 12:48:46 +0530

    Update my-scenario.js

--- my-scenario.js
+++ my-scenario.js
@@ -1,1 +1,9 @@
+// in my-scenario.js
+const visit = async (page) => {
+    await page.goto('', { waitUntil: 'networkidle' }); // Go to the baseUrl
+    await page.waitForTimeout(3000); // Wait for 3 seconds
+    await page.scrollToElement('footer'); // Scroll to the footer (if present)
+    await page.waitForNetworkIdle(); // Wait every request has been answered as a normal user.
+};
 
+module.exports = visit;
